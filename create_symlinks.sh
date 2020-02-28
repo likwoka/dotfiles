a2bfb1821f30b07634600f7803522a03f008169c
Alex Li <likwoka@gmail.com>
2020-02-28 00:33:01 -0500

    Add comment

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# as of 2020 02 28
+# Assume the dotfiles repo is checkout from git, then run 
+# this script to create symlinks in your home folder.
+# Might got error if there is already a file with same name
+# in your home folder!
+
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.inputrc ~/.inputrc
